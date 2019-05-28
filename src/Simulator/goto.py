a56f06f559503330cbda86360fc08738ec029811
mjiang24 <mjiang24@illinois.edu>
2019-05-28 15:48:44 -0500

    add comment:

--- src/Simulator/goto.py
+++ src/Simulator/goto.py
@@ -9,12 +9,18 @@ from geometry_msgs.msg import Point, Twist
 from math import atan2, sqrt
 from robot import Robot
 
+'''
+    This class is used for moving multiple robots towards a specific point with physical controller
+'''
 class GoTo():
+    '''
+        Initiates robots
+    '''
     def __init__(self):
         rospy.init_node('GoTo_test', anonymous=False)
         self.numberOfRobots = 2
-        self.robots = []
-        self.complete = []
+        self.robots = []        # Robot list
+        self.complete = []      # Indicator of completion of tasks
         for i in range(self.numberOfRobots):
             self.robots.append(Robot(i+1))
             self.complete.append(0)
@@ -26,15 +32,19 @@ class GoTo():
         rospy.loginfo("Ready to move. To stop Robot , press CTRL + C")
         rospy.loginfo("Robot1 goes to (%f, %f)", pos1['x'], pos2['y'])
         rospy.loginfo("Robot2 goes to (%f, %f)", pos2['x'], pos2['y'])
+        # Set rate
         r = rospy.Rate(10)
+        # Twist message
         move_cmd = Twist()
 
-        # Set up goal
+        # Set up goal for each robot
         for i in range(self.numberOfRobots):
             self.robots[i].goal.x = goals[i]['x']
             self.robots[i].goal.y = goals[i]['y']
 
+        # Main while loop for moving the robots
         while not rospy.is_shutdown():
+            # Check completion requirement
             if sum(self.complete) == self.numberOfRobots:
                 return 1
 
@@ -42,26 +52,30 @@ class GoTo():
                 diff_x = self.robots[i].goal.x - self.robots[i]._x
                 diff_y = self.robots[i].goal.y - self.robots[i]._y
                 angle_to_goal = atan2(diff_y, diff_x)
+                # If distance to goal is small enough, DONE!
                 if sqrt(diff_x*diff_x + diff_y*diff_y) < 0.1:
-                    self.complete[i] = 1 
+                    self.complete[i] = 1
+                # If orientation difference is too big, ROTATE!
                 elif abs(angle_to_goal - self.robots[i]._theta) > 0.2:
-                    # rospy.loginfo("atg is %f, theta is %f, diff is %f", angle_to_goal, self._theta, abs(angle_to_goal - self._theta))
                     move_cmd.linear.x = 0.0
                     move_cmd.angular.z = 0.3
+                # Otherwise, move towards goal
                 else:
                     move_cmd.linear.x = 0.5
                     move_cmd.angular.z = 0.0
-            
+
+                # Send Twist message
                 self.robots[i].pub.publish(move_cmd)
 
+            # Let robot to go
             r.sleep()
 
     def shutdown(self):
         rospy.loginfo("Stop Robot")
-	    # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
         for i in range(self.numberOfRobots):
             self.robots[i].pub.publish(Twist())
-	    # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
 
 if __name__ == '__main__': 
--- src/Simulator/robot.py
+++ src/Simulator/robot.py
@@ -6,24 +6,38 @@ from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point, Twist
 from math import atan2, sqrt
 
+'''
+    The robot class used in Gazebo
+'''
 class Robot():
-    def __init__(self, number):
-        # Robot's position and orientation inforamtion
+    '''
+        This function construct a robot initialized to (0,0).
+        Args: index - index of current robot
+        Return: None
+    '''
+    def __init__(self, index):
+        # Robot's position and orientation information
         self._x = 0.0
         self._y = 0.0
         self._theta = 0.0
         self.goal = Point()
 
         # Set up subscriber and publisher
-        my_number = "/robot" + str(number)
-        self.sub = rospy.Subscriber(my_number+"/odom", Odometry, self.newPos)
-        self.pub = rospy.Publisher(my_number+"/cmd_vel_mux/input/navi", Twist, queue_size=10)
+        my_index = "/robot" + str(index)
+        self.sub = rospy.Subscriber(my_index+"/odom", Odometry, self.newPos)    # odom gives current position estimation
+        self.pub = rospy.Publisher(my_index+"/cmd_vel_mux/input/navi", Twist, queue_size=10)    # Move robots
 
-
+    '''
+        This function is used for subscription to new odometry messages  
+        Args: msg - nav_msg for pose and quaternion
+        Returns: None
+    '''
     def newPos(self, msg):
+        # Update pose
         self._x = msg.pose.pose.position.x 
         self._y = msg.pose.pose.position.y 
 
+        # Update orientation, theta
         quat = msg.pose.pose.orientation
         (_, _, self._theta) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
 
