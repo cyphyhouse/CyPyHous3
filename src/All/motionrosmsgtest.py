8800d1ff3f12c85149171ef43d6c2d7e843aafe0
ritwika314 <mim.ghosh@gmail.com>
2019-05-29 16:41:10 -0500

    debugging publish with private field as a property, publish once in for loop. farcical experiment

--- src/All/motionrosmsgtest.py
+++ src/All/motionrosmsgtest.py
@@ -10,6 +10,6 @@ dest1 = Pose()
 dest1.position.x, dest1.position.y,dest1.position.z = 1.,2.,3.
 
 quad_m.start()
-for i in range(0,20):
+for i in range(0,1):
     quad_m.goTo(dest1)
     time.sleep(0.5)
