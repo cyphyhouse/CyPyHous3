a5c19a9d658b8d397f734db2e0edd20af55a0cb9
ritwika314 <mim.ghosh@gmail.com>
2019-05-29 16:37:02 -0500

    debugging publish with public field, repeated publish

--- src/All/motionrosmsgtest.py
+++ src/All/motionrosmsgtest.py
@@ -1,5 +1,5 @@
 # tests whether the goto function publishes the correct ros messages
-
+import time
 import motionautomaton
 
 from geometry_msgs.msg import Pose
@@ -10,5 +10,6 @@ dest1 = Pose()
 dest1.position.x, dest1.position.y,dest1.position.z = 1.,2.,3.
 
 quad_m.start()
-
-quad_m.goTo(dest1)
+for i in range(0,20):
+    quad_m.goTo(dest1)
+    time.sleep(0.5)
