42ba582bc18588dc8f745881bdd3bdd7aba3fecd
ritwika314 <mim.ghosh@gmail.com>
2019-06-24 09:42:37 -0500

    koord

--- src/basicFollowApp.py
+++ src/basicFollowApp.py
@@ -1,9 +1,7 @@
-import time
-
-from geometry_msgs.msg import Pose
-
 from agentThread import AgentThread
+from geometry_msgs.msg import Pose
 from gvh import Gvh
+import time
 
 
 class BasicFollowApp(AgentThread):
@@ -13,11 +11,41 @@ class BasicFollowApp(AgentThread):
         self.start()
 
     def run(self):
+        #local int tries = 1,
+        # pos dest1 = new pos(0.,0.,1)
+        # pos land = new pos(0.,0.,0.)
+
+        tries = 1
+
         dest1 = Pose()
         dest1.position.x, dest1.position.y, dest1.position.z = 0., 0., 1.
         land = Pose()
         land.position.x, land.position.y, land.position.z = 0., 0., 0.
+        while not self.stopped():
+            time.sleep(1.0)
 
-        self.agent_gvh.moat.goTo(dest1)
-        time.sleep(1)
-        self.agent_gvh.moat.goTo(land)
+
+
+            # event 1 : pre (tries == 1) .
+            # eff : Motion.target = dest1
+            # tries = 2
+
+            #event 2: pre(tries == 2)
+            # eff : Motion.target = dest2
+            # tries = 3
+            # stop
+
+
+            if tries == 1:
+                self.agent_gvh.moat.goTo(dest1)
+                tries = 2
+                continue
+            if tries == 2:
+                self.agent_gvh.moat.goTo(land)
+                tries = 3
+                self.stop()
+                continue
+
+
+
+
--- src/datatypes.py
+++
@@ -1,9 +0,0 @@
-
-def mtype(i):
-    if type(i) == type([]):
-        print("here")
-        return (type(i), mtype(i[0]))
-    else:
-        return (type(i))
-
-
