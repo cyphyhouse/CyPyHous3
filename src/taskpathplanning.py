acaf5d2a3c58da282b4b1b469680b191eb1dc026
ritwika314 <mim.ghosh@gmail.com>
2019-07-09 13:40:50 -0500

    update

--- src/taskpathplanning.py
+++ src/taskpathplanning.py
@@ -99,7 +99,7 @@ class AgentCreation(AgentThread):
                             tasks[i].assigned_to = self.pid()
                             # print("assigned task", tasks[i].id, "to ", self.pid())
                             mytask = tasks[i]
-                            print(self.agent_gvh.moat)
+                            print(self.agent_gvh.moat.planner)
                             testroute = self.agent_gvh.moat.find_path(mytask.location)
 
                             print(testroute)
