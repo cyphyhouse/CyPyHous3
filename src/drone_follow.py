37dcb7c25ed160c9ad3918f17a40f069e4b5b66f
ritwika314 <mim.ghosh@gmail.com>
2019-07-15 16:12:27 -0500

    basicfollwo for drone

--- src/drone_follow.py
+++ src/drone_follow.py
@@ -1,6 +1,6 @@
 import time
 
-import motionAutomaton
+import motionAutomaton_drone
 import rrt_star
 from agentThread import AgentThread
 from geometry_msgs.msg import Pose
@@ -11,7 +11,7 @@ class BasicFollowApp(AgentThread):
 
     def __init__(self, pid: int, num_bots: int):
         agent_gvh = Gvh(pid, num_bots)
-        agent_gvh.moat = motionAutomaton.MotionAutomaton(rrt_star.RRT, pid, 'hotdec_car', 10)
+        agent_gvh.moat = motionAutomaton_drone.MotionAutomaton(None, pid, 'cyphyhousecopter', 10)
         super(BasicFollowApp, self).__init__(agent_gvh, None)
         self.start()
 
@@ -23,9 +23,9 @@ class BasicFollowApp(AgentThread):
         tries = 1
 
         dest1 = Pose()
-        dest1.position.x, dest1.position.y, dest1.position.z = 2., 1., 0.
+        dest1.position.x, dest1.position.y, dest1.position.z = 2., 1., 1.
         dest2 = Pose()
-        dest2.position.x, dest2.position.y, dest2.position.z = -1., -1., 0.
+        dest2.position.x, dest2.position.y, dest2.position.z = -1., -1., 1.
         dest3 = Pose()
         dest3.position.x, dest3.position.y, dest3.position.z = 1., 2., 0.
 
@@ -41,33 +41,24 @@ class BasicFollowApp(AgentThread):
             # eff : Motion.target = dest2
             # tries = 3
             # stop
-            a = rrt_star.RRT()
 
             if tries == 1:
-                a.plan([self.agent_gvh.moat.position.position.x,
-                        self.agent_gvh.moat.position.position.y], [dest1.position.x, dest1.position.y])
-                path1 = a.Planning()
-                self.agent_gvh.moat.follow_path(path1)
+
+                self.agent_gvh.moat.goTo(dest1)
                 # self.agent_gvh.moat.follow_path([dest1])
                 time.sleep(5)
                 tries = 2
                 continue
             if tries == 2:
-                a.plan([self.agent_gvh.moat.position.position.x,
-                        self.agent_gvh.moat.position.position.y], [dest2.position.x, dest2.position.y])
-                path2 = a.Planning()
-                self.agent_gvh.moat.follow_path(path2)
+
+                self.agent_gvh.moat.goTo(dest2)
                 # self.agent_gvh.moat.follow_path([dest2])
 
                 tries = 3
                 time.sleep(5)
                 continue
             if tries == 3:
-                a.plan([self.agent_gvh.moat.position.position.x,
-                        self.agent_gvh.moat.position.position.y],
-                       [dest3.position.x, dest3.position.y])
-                path3 = a.Planning()
-                self.agent_gvh.moat.follow_path(path3)
+                self.agent_gvh.moat.goTo(dest3)
                 # self.agent_gvh.moat.follow_path([dest2])
                 tries = 4
                 time.sleep(5)
