139d3b96dba6d0bd10ee0cf62dd2522dfaa2a4fd
ritwika314 <mim.ghosh@gmail.com>
2019-07-24 15:58:35 -0500

    update

--- src/apps/test_2car_follow.py
+++ src/apps/test_2car_follow.py
@@ -1,16 +1,10 @@
-from src.apps.two_car_follow_path import BasicFollowApp
+from src.apps.addnums import AddNums
 from src.functionality.base_mutex_handler import BaseMutexHandler
 from src.config.configs import AgentConfig, default_car_moat_config
-from src.motion.rrt_star import RRT
 
-m = default_car_moat_config('f1car')
-m.planner = RRT(goal_sample_rate=20)
-a = AgentConfig(1, 2, "", 2000, mh=BaseMutexHandler(False,1))
-app = BasicFollowApp(a, m)
+bots = 2
 
-
-m = default_car_moat_config('hotdec_car')
-m.planner = RRT(goal_sample_rate=20)
-a = AgentConfig(0, 2, "", 2000, is_leader=True, mh= BaseMutexHandler(True,0))
-app = BasicFollowApp(a, m)
+a = AgentConfig(1, bots, "", 2000, [], BaseMutexHandler(False, 1))
+a = AgentConfig(2, bots, "", 2000, [], BaseMutexHandler(True, 2))
+app = AddNums(a)
 
