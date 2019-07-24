582f04e2980c85c02a87522b4e0951be1ac7b8b9
ritwika314 <mim.ghosh@gmail.com>
2019-07-24 15:33:10 -0500

    update

--- src/apps/test_2car_follow.py
+++ src/apps/test_2car_follow.py
@@ -2,15 +2,15 @@ from src.apps.two_car_follow_path import BasicFollowApp
 from src.config.configs import AgentConfig, default_car_moat_config
 from src.motion.rrt_star import RRT
 
-m = default_car_moat_config('hotdec_car')
+m = default_car_moat_config('f1car')
 m.planner = RRT(goal_sample_rate=20)
 a = AgentConfig(1, 2, "", 2000)
 app = BasicFollowApp(a, m)
 
-'''
+
 m = default_car_moat_config('hotdec_car')
 obstacles = [Obs(0, 0, 0.75)]
 m.planner = RRT(goal_sample_rate=20)
-a = AgentConfig(0, 2, "", 2000)
+a = AgentConfig(0, 2, "", 2000, is_leader=True)
 app = BasicFollowApp(a, m)
-'''
+
