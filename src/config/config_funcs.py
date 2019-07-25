266e5b13bfb91ce75d9c77fcd9db94bb1f9e1a10
Hsieh, Chiao <chsieh16@illinois.edu>
2019-07-25 16:20:37 -0500

    Optionally create Moat class and remove gen_config_old etc.

--- src/config/config_funcs.py
+++ src/config/config_funcs.py
@@ -9,34 +9,46 @@ def __validate(cfg) -> bool:
     return True
 
 
-def get_configs(config_filename: str) -> Tuple[AgentConfig, MoatConfig]:
+def get_configs(config_filename: str) \
+        -> Tuple[AgentConfig, Optional[MoatConfig]]:
     with open(config_filename) as f:
         cfg = yaml.load(f)
     if not __validate(cfg):
         raise ValueError("Invalid YAML file" + config_filename)
 
-    # Make AgentConfig
-    from .config_dicts import mutex_handler_dict, moat_class_dict
     agent_dict = cfg['agent']
     device_dict = cfg['device']
+    is_using_moat = (agent_dict.get('motion_automaton', None) is not None)
 
+    # Make AgentConfig
     pid = agent_dict['pid']
     is_leader=(cfg['leader_pid'] == agent_dict['pid'])
+    if not is_using_moat:
+        moat_class = None
+    else:
+        from .config_dicts import moat_class_dict
+        moat_class = moat_class_dict[agent_dict['motion_automaton']]
+
+    from .config_dicts import mutex_handler_dict
+    mh = mutex_handler_dict[cfg['mutex_handler']](pid, is_leader)
 
     agent_conf = AgentConfig(
         # Shared configs
         bots=cfg['num_agents'],
         # Device specific configs
-        moat_class=moat_class_dict[agent_dict['motion_automaton']],  # TODO choose Motion Automaton only when necessary
+        moat_class=moat_class,
         rip=device_dict['ip'],
         rport=device_dict['port'],
         # Agent specific configs
         pid=pid,
         is_leader=is_leader,
-        mh=mutex_handler_dict[cfg['mutex_handler']](pid, is_leader),
+        mh=mh,
         plist=agent_dict['plist'],
     )
 
+    if not is_using_moat:
+        return agent_conf, None
+    # else:
     from .config_dicts import planner_dict, bot_type_dict, msg_type_dict
     moat_conf = MoatConfig(
         bot_name=device_dict['bot_name'],
@@ -54,47 +66,7 @@ def get_configs(config_filename: str) -> Tuple[AgentConfig, MoatConfig]:
     return agent_conf, moat_conf
 
 
-def get_configs_old(configfilename, agentnum, moatnum):
-    f = open(configfilename)
-    cfg = yaml.load(f)
-    agent_config = None
-    moat_config = None
-    if agentnum >= 0:
-        agent_config = mk_agent_config(cfg['agents'][agentnum])
-    if moatnum >=0:
-        moat_config = mk_moat_config(cfg['motion_automaton'][moatnum])
-    return [agent_config, moat_config]
-
-
-def mk_agent_config(agent_dict):
-    from src.config.config_dicts import mutex_handler_dict, moat_class_dict
-    agent_dict['mutex_handler'] = mutex_handler_dict[agent_dict['mutex_handler']]
-    agent_dict['moat_class'] = moat_class_dict[agent_dict['moat_class']]
-    a = AgentConfig(agent_dict['pid'], agent_dict['bots'], agent_dict['rip'], agent_dict['rport'], agent_dict['plist'],
-                    agent_dict['mutex_handler'](agent_dict['pid'], agent_dict['is_leader']), agent_dict['is_leader'],
-                    agent_dict['moat_class'])
-    return a
-
-
-def mk_moat_config(moat_dict):
-    from src.config.config_dicts import planner_dict, bot_type_dict, msg_type_dict
-    moat_dict['planner'] = planner_dict[moat_dict['planner']]
-    moat_dict['pos_msg_type'] = msg_type_dict[moat_dict['pos_msg_type']]
-    moat_dict['rchd_msg_type'] = msg_type_dict[moat_dict['rchd_msg_type']]
-    moat_dict['bot_type'] = bot_type_dict[moat_dict['bot_type']]
-    return MoatConfig(**moat_dict)
-
-
 if __name__ == "__main__":
     import sys
-
     new_conf = get_configs(sys.argv[1])
-    old_conf = get_configs_old(sys.argv[2], 1, 1)
-
-    assert new_conf[0].__dict__.keys() == old_conf[0].__dict__.keys()
-    assert all(new_conf[0].__dict__[k] == old_conf[0].__dict__[k] \
-            for k in new_conf[0].__dict__.keys() if k != 'mutex_handler')
-
-    assert new_conf[1].__dict__ == old_conf[1].__dict__, \
-        "New: " + str(new_conf[1].__dict__) + "\n" + \
-        "Old: " + str(old_conf[1].__dict__) + "\n"
+    print(new_conf)
