1f38df447fbac4d080f2cbf4a6f54b2a2ede31af
ritwika314 <mim.ghosh@gmail.com>
2019-07-25 16:10:03 -0500

    two cars

--- src/apps/two_car_follow_path.py
+++ src/apps/two_car_follow_path.py
@@ -18,7 +18,7 @@ class BasicFollowApp(AgentThread):
         self.create_aw_var('pointnum', int, 0)
         self.initialize_lock('singlelock')
         self.locals['dest'] = [pos3d(2., 2., 0.), pos3d(2., -2., 0.), pos3d(-2., -2., 0.), pos3d(-2., 2., 0.)]
-        self.locals['obstacles'] = [Obs(3., 3., 0.75)]
+        self.locals['obstacles'] = []
         self.locals['going'] = False
 
     def loop_body(self):
