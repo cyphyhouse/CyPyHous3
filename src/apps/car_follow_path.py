aa7482584257f332676cf7f909d61a31c40927d6
ritwika314 <mim.ghosh@gmail.com>
2019-07-29 16:33:10 -0500

    update maybe

--- src/apps/car_follow_path.py
+++ src/apps/car_follow_path.py
@@ -173,8 +173,8 @@ class BasicFollowApp(AgentThread):
 
 
 
-m = default_car_moat_config('hotdec_car')
+m = default_car_moat_config('f1car')
 obstacles = [Obs(1, 0, 0.75), Obs(0, -2, 0.75), Obs(-2,0,0.75)]
-m.planner = RRT_DUBINS(goal_sample_rate=15, max_iter=100)
+m.planner = RRT(goal_sample_rate=15, max_iter=100)
 a = AgentConfig(1, 1, "", 2000)
 app = BasicFollowApp(a, m)
