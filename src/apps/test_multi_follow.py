af068a34bb62f9376ab4c82484a51c7f454aaa11
gossea <gossea@protonmail.com>
2019-07-30 15:18:08 -0400

    Correctly name "f1car"

--- src/apps/test_multi_follow.py
+++ src/apps/test_multi_follow.py
@@ -11,7 +11,7 @@ mharg
 a1 = AgentConfig(0, bots, "", 2000, [], BaseMutexHandler, , mhargs=[False,0])
 a2 = AgentConfig(1, bots, "", 2000, [], BaseMutexHandler, is_leader=True, mhargs=[True, 1])
 m1 = default_car_moat_config('hotdec_car')
-m2 = default_car_moat_config('ficar')
+m2 = default_car_moat_config('f1car')
 m2.planner = RRT_CAR(goal_sample_rate=30)
 app1 = BasicFollowApp(a1, m1)
 app2 = BasicFollowApp(a2, m2)
