752e059eb60843af5e6135065ad64c02e126b180
ritwika314 <mim.ghosh@gmail.com>
2019-08-01 15:16:30 -0500

    update

--- src/apps/taskmain.py
+++ src/apps/taskmain.py
@@ -7,7 +7,7 @@ from src.motion.simpleplanner import SimplePlanner
 from src.motion.moat_test_drone import MoatTestDrone
 from src.motion.moat_test_car import MoatTestCar
 
-bots = 2
+bots = 1
 mhargs = [[True,0], [False, 1], [False,2], [False,3]]
 plist = []
 r_port = 2000
