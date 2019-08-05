843aa35b3466a7aebadf59d119b15ebeb0f86332
ritwika314 <mim.ghosh@gmail.com>
2019-08-05 16:44:47 -0500

    testing

--- src/motion/rrt_star.py
+++ src/motion/rrt_star.py
@@ -41,6 +41,7 @@ class RRT(Planner):
         RRT* Path Planning
         search_until_max_iter: Search until max iteration for path improving or not
         """
+        print("testing",self,start, end)
         if obstacle_list is None:
             obstacle_list = []
         print("here, start:",start, "end:",end)
