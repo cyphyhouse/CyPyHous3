648b2584da1885c54b9d032ca8df32d64cbb72d6
ritwika314 <mim.ghosh@gmail.com>
2019-08-05 16:30:36 -0500

    reverting stupidity

--- src/apps/taskmain_with_configs.py
+++ src/apps/taskmain_with_configs.py
@@ -3,9 +3,11 @@ import sys
 from src.config.config_funcs import get_configs
 from src.config.configs import default_car_moat_config
 from src.apps.taskapp import TaskApp
+from src.motion.rrt_star import RRT
 
 a1, m1 = get_configs(config_filename=sys.argv[1])
 m0 = default_car_moat_config('hotdec_car')
-
+m0.planner = RRT
+print(m0.planner == m1.planner)
 # print(m1.pos_msg_type)
-TaskApp(a1, m1)
+#TaskApp(a1, m1)
