9c43f831ceadefd5f2ae9723ef85dfcdba4e3ad2
ritwika314 <mim.ghosh@gmail.com>
2019-08-05 16:20:57 -0500

    changed task main

--- src/apps/taskmain_with_configs.py
+++ src/apps/taskmain_with_configs.py
@@ -2,6 +2,7 @@ import sys
 
 from src.config.config_funcs import get_configs
 from src.config.configs import default_car_moat_config
+from src.apps.taskapp import TaskApp
 
 a1, m1 = get_configs(config_filename=sys.argv[1])
 m0 = default_car_moat_config('hotdec_car')
