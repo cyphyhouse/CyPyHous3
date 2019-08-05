1f0a347fcf8b37f2d9ff81f0a99639e3a4c833d9
ritwika314 <mim.ghosh@gmail.com>
2019-08-05 16:54:29 -0500

    update

--- src/apps/taskmain_with_configs.py
+++ src/apps/taskmain_with_configs.py
@@ -2,11 +2,8 @@ import sys
 
 from src.apps.taskapp import TaskApp
 from src.config.config_funcs import get_configs
-from src.motion.moat_test_car import MoatTestCar
 from src.motion.rrt_star import RRT
 
 a_c, m_c = get_configs(sys.argv[1])
 m_c.planner = RRT(goal_sample_rate=15)
-
-a_c.moat_class = MoatTestCar
 app = TaskApp(a_c, m_c)
