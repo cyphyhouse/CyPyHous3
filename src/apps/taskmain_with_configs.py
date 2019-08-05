92230ef9ffe292053642c8da3226cb77e3d4039b
ritwika314 <mim.ghosh@gmail.com>
2019-08-05 16:39:09 -0500

    testing

--- src/apps/taskmain_with_configs.py
+++ src/apps/taskmain_with_configs.py
@@ -29,6 +29,6 @@ a1, m1 = get_configs(config_filename=sys.argv[1])
 m0 = default_car_moat_config('hotdec_car')
 m0.planner = RRT
 a0.moat_class = MoatTestCar
-print(m0. == m1, a0 == a1)
+print( a0 == a1)
 # print(m1.pos_msg_type)
 #TaskApp(a1, m1)
