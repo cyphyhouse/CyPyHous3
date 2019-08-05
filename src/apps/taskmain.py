0032b92fb54882ec6475f608775a834d8358caa2
gossea <gossea@protonmail.com>
2019-08-05 16:00:25 -0400

    Update task planners

--- src/apps/taskmain.py
+++ src/apps/taskmain.py
@@ -4,6 +4,7 @@ from src.functionality.base_mutex_handler import BaseMutexHandler
 from src.motion.rrt_star import RRT as RRT_CAR
 from src.motion.rrt_star_drone import RRT as RRT_DRONE
 from src.motion.simpleplanner import SimplePlanner
+from src.motion.reeds_shepp_planner import Reeds_Shepp_Planner
 from src.motion.moat_test_drone import MoatTestDrone
 from src.motion.moat_test_car import MoatTestCar
 
@@ -26,12 +27,12 @@ m0 = default_car_moat_config('hotdec_car')
 m1 = default_car_moat_config('f1car')
 m2 = default_qc_moat_config('cyphyhousecopter')
 m3 = default_qc_moat_config('cyphyhousecopter1')
-m0.planner = RRT_CAR(goal_sample_rate=15)
+m0.planner = Reeds_Shepp_Planner()
 m1.planner = RRT_CAR(goal_sample_rate=15)
 m2.planner = RRT_DRONE()
 m3.planner = RRT_DRONE()
 
 #change this line
-a, m = a1, m1
+a, m = a0, m0
 a.moat_class = MoatTestCar
 app = TaskApp(a,m)
