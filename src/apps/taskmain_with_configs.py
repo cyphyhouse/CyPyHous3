4b08aa5492f8099121de7e98611fc8ccf20f8e78
ritwika314 <mim.ghosh@gmail.com>
2019-08-05 16:05:02 -0500

    update

--- src/apps/taskmain_with_configs.py
+++ src/apps/taskmain_with_configs.py
@@ -1,17 +1,8 @@
 import sys
 
 from src.config.config_funcs import get_configs
-from src.config.configs import AgentConfig, default_car_moat_config
-from src.functionality.base_mutex_handler import BaseMutexHandler
+from src.apps.taskapp import TaskApp
 
-bots = 2
-mhargs = [[True, 0], [False, 1], [False, 2], [False, 3]]
-plist = []
-r_port = 2000
-r_ip = ""
-pid = [0, 1, 2, 3]
-
-m0 = default_car_moat_config('hotdec_car')
 a1, m1 = get_configs(config_filename=sys.argv[1])
-print(m0,"\n\n",m1)
-# TaskApp(a1, m1)
+
+TaskApp(a1, m1)
