5a7676084e20aa7b58189cff6afdcc499c7077ed
Hsieh, Chiao <chsieh16@illinois.edu>
2019-08-06 14:21:29 -0500

    Rename the packge as src for now

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='CyPyHous3',
+setup(name='src',
       version='0.1',
       description='CyPyHous3 Python Middleware for Distributed Robotic Systems',
       author='Ritwika Ghosh',
       license='MIT',
-      packages=['cypyhous3'],
-      package_dir={'cypyhous3': 'src'},
+      packages=['src'],
       install_requires=[
           'matplotlib',
           'numpy',
