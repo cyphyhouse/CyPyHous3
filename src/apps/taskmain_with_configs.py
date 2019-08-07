fb840b4bbd7cee8125e15e5d1ff19a447f90a217
ritwika314 <mim.ghosh@gmail.com>
2019-08-07 14:49:54 -0500

    doing task

--- src/apps/taskmain_with_configs.py
+++ src/apps/taskmain_with_configs.py
@@ -6,5 +6,4 @@ from src.apps.taskapp import TaskApp
 from src.config.config_funcs import get_configs
 
 a_c, m_c = get_configs(sys.argv[1])
-print(a_c,"\n\n", m_c)
-#app = TaskApp(a_c, m_c)
+app = TaskApp(a_c, m_c)
