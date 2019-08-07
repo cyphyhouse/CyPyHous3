9b0356628f31f3f873005491ce85a30e82c56efc
ritwika314 <mim.ghosh@gmail.com>
2019-08-07 14:46:38 -0500

    app with configs

--- src/apps/taskmain_with_configs.py
+++ src/apps/taskmain_with_configs.py
@@ -4,8 +4,7 @@ import sys
 
 from src.apps.taskapp import TaskApp
 from src.config.config_funcs import get_configs
-from src.motion.rrt_star import RRT
 
 a_c, m_c = get_configs(sys.argv[1])
-m_c.planner = RRT(goal_sample_rate=15)
-app = TaskApp(a_c, m_c)
+print(a_c,"\n\n", m_c)
+#app = TaskApp(a_c, m_c)
