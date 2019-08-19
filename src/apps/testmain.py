d1cf3cbf64ddadb95d7f075726c02a3de1916d8a
ritwika314 <mim.ghosh@gmail.com>
2019-08-19 15:42:38 -0500

    Testing

--- src/apps/testmain.py
+++ src/apps/testmain.py
@@ -4,8 +4,8 @@ from src.functionality.base_mutex_handler import BaseMutexHandler
 
 bots = 2
 
-a1 = AgentConfig(1, bots, "", rport=2000, plist=[], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
-a2 = AgentConfig(2, bots, "", rport=2000, plist=[], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
+a1 = AgentConfig(1, bots, "", rport=2001, plist=[2001,2002,2003], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
+a2 = AgentConfig(2, bots, "", rport=2002, plist=[2001,2002,2003], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
 a3 = AgentConfig(3, bots, "", rport=2003, plist=[2001,2002,2003], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
 
 app1 = AddNums(a1)
