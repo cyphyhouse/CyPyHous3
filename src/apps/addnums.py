fc541303c25db4df22c925c6bb8fb485033ab1e0
ritwika314 <mim.ghosh@gmail.com>
2019-08-19 15:23:24 -0500

    checking mutex_holder

--- src/apps/addnums.py
+++ src/apps/addnums.py
@@ -21,6 +21,7 @@ class AddNums(AgentThread):
     def loop_body(self):
         import time
         time.sleep(0.1)
+        print("numadded, should be 4 at the end",self.read_from_shared('numadded', None))
 
         if not self.locals['added'] >= 2:
             if not self.lock('adding'):
