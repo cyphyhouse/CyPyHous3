befd376336bd29d0d9ea693eb84db7a1288c0435
ritwika314 <mim.ghosh@gmail.com>
2019-08-22 12:45:01 -0500

    Testing

--- src/apps/taskwithoutgoing.py
+++ src/apps/taskwithoutgoing.py
@@ -1,21 +1,25 @@
 import time
 
-from src.config.configs import AgentConfig
+from src.config.configs import AgentConfig, MoatConfig
 from src.harness.agentThread import AgentThread
+from src.motion.deconflict import clear_path
 from src.objects.udt import get_tasks
 
 
 class TaskApp(AgentThread):
 
-    def __init__(self, agent_config: AgentConfig):
-        super(TaskApp, self).__init__(agent_config, None)
+    def __init__(self, agent_config: AgentConfig, moat_config: MoatConfig):
+        super(TaskApp, self).__init__(agent_config, moat_config)
         self.start()
 
     def initialize_vars(self):
         self.initialize_lock('pick_route')
-        self.agent_gvh.create_aw_var('tasks', list, [0 for i in get_tasks(taskfile='src/apps/tasks.txt')])
+        self.agent_gvh.create_aw_var('tasks', list, get_tasks(taskfile='src/apps/tasks.txt'))
+        self.agent_gvh.create_ar_var('route', list, [self.agent_gvh.moat.position])
         self.locals['my_task'] = None
+        self.locals['test_route'] = None
         self.locals['doing'] = False
+        self.locals['tasks'] = []
         self.locals['obstacles'] = []
 
     def loop_body(self):
