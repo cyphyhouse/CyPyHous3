8c11f39778853c20199cd88aa8dc60204d58f51b
ritwika314 <mim.ghosh@gmail.com>
2019-08-22 13:04:57 -0500

    Testing

--- src/apps/taskwithoutgoing.py
+++ src/apps/taskwithoutgoing.py
@@ -49,6 +49,7 @@ class TaskApp(AgentThread):
                         self.locals['tasklist'][i] = self.locals['my_task']
                         self.locals['tasks'][i] = 1
                         self.agent_gvh.put('tasks', self.locals['tasks'])
+                        print(self.locals['test_route'])
                         self.agent_gvh.put('route', self.locals['test_route'], self.pid())
                         #self.agent_gvh.moat.follow_path(self.locals['test_route'])
 
