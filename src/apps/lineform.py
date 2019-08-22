c23e55133b13a3ae30d76e89f9fa7c8b2ee87b25
ritwika314 <mim.ghosh@gmail.com>
2019-08-22 12:06:06 -0500

    lineform

---
+++ src/apps/lineform.py
@@ -0,0 +1,20 @@
+from src.config.configs import AgentConfig, MoatConfig
+from src.harness.agentThread import AgentThread
+from src.motion.pos_types import pos3d, mid_pt
+
+
+class LineForm(AgentThread):
+
+    def __init__(self, agent_config: AgentConfig, moat_config: MoatConfig):
+        super(LineForm, self).__init__(agent_config, moat_config)
+        self.start()
+
+    def initialize_vars(self):
+        self.agent_gvh.create_ar_var('mypos', type(pos3d), self.agent_gvh.moat.position)
+        self.locals['tries'] = 1
+
+    def loop_body(self):
+        while (self.locals['i'] < 10):
+            if not (self.pid() == 0 or self.pid() == self.num_agents() - 1):
+                self.agent_gvh.moat.goTo(mid_pt(self.read_from_shared('mypos', self.pid() - 1),
+                                                self.read_from_shared('mypos', self.pid() + 1)))
