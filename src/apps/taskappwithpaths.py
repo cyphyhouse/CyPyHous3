ebad2fab9e5586e0e4a69d1f49fd9fd815b5936f
ritwika314 <mim.ghosh@gmail.com>
2019-08-21 20:16:34 -0500

    update

---
+++ src/apps/taskappwithpaths.py
@@ -0,0 +1,61 @@
+import time
+
+from src.config.configs import AgentConfig, MoatConfig
+from src.harness.agentThread import AgentThread
+from src.motion.deconflict import clear_path
+from src.objects.udt import get_tasks
+
+
+class TaskApp(AgentThread):
+
+    def __init__(self, agent_config: AgentConfig, moat_config: MoatConfig):
+        super(TaskApp, self).__init__(agent_config, moat_config)
+        self.start()
+
+    def initialize_vars(self):
+        self.initialize_lock('pick_route')
+        self.locals['tasklist'] = get_tasks(taskfile='src/apps/tasks.txt')
+        self.agent_gvh.create_aw_var('tasks', list, [0 for i in range(len(self.locals['tasklist']))])
+        self.agent_gvh.create_ar_var('route', list, [self.agent_gvh.moat.position])
+        self.locals['my_task'] = None
+        self.locals['test_route'] = None
+        self.locals['doing'] = False
+        self.locals['tasks'] = []
+        self.locals['obstacles'] = []
+
+    def loop_body(self):
+        if not self.locals['doing']:
+            if sum([int(a.assigned) for a in self.read_from_shared('tasks', None)]) == len(
+                    self.read_from_shared('tasks', None)):
+                self.stop()
+                return
+
+            if self.lock('pick_route'):
+                time.sleep(0.2)
+                self.locals['tasks'] = self.read_from_shared('tasks', None)
+                print("assignment map is", self.locals['tasks'])
+                for i in range(len(self.locals['tasks'])):
+                    if not self.locals['tasks'][i] == 1:
+                        self.locals['my_task'] = self.locals['tasklist'][i]
+                        print("going to task", i)
+
+                        self.locals['test_route'] = self.agent_gvh.moat.planner.find_path(self.agent_gvh.moat.position,
+                                                                                          self.locals[
+                                                                                              'my_task'].location,
+                                                                                          self.locals['obstacles'])
+
+                        self.locals['doing'] = True
+                        self.locals['my_task'].assign(self.pid())
+                        self.locals['tasklist'][i] = self.locals['my_task']
+                        self.locals['tasks'][i] = 1
+                        self.agent_gvh.put('tasks', self.locals['tasks'])
+                        self.agent_gvh.put('route', self.locals['test_route'], self.pid())
+                        #self.agent_gvh.moat.follow_path(self.locals['test_route'])
+
+                        self.unlock('pick_route')
+                        break
+        else:
+            if self.locals['my_task'] is not None:
+                self.locals['my_task'] = None
+            self.locals['doing'] = False
+            return
--- test1.py
+++ test1.py
@@ -1,12 +1,12 @@
-from src.apps.taskwithoutgoing import TaskApp
+from src.apps.taskappwithpaths import TaskApp
 from src.config.configs import AgentConfig
 from src.functionality.base_mutex_handler import BaseMutexHandler
 
 bots = 4
 
-a1 = AgentConfig(1, bots, "", rport=2001, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
-a2 = AgentConfig(2, bots, "", rport=2002, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
-a3 = AgentConfig(3, bots, "", rport=2003, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
-a4 = AgentConfig(4, bots, "", rport=2004, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,4])
+a1 = AgentConfig(1, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
+a2 = AgentConfig(2, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
+a3 = AgentConfig(3, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
+a4 = AgentConfig(4, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=False, mhargs=[False,4])
 
 app1 = TaskApp(a1)
