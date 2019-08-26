77e50020292c25e3ea149cd64c83b3123523fbed
ritwika314 <mim.ghosh@gmail.com>
2019-08-26 14:36:45 -0500

    tesing

--- test2.py
+++ test2.py
@@ -6,7 +6,7 @@ from src.functionality.base_mutex_handler import BaseMutexHandler
 bots = 4
 
 a0 = AgentConfig(0, bots, "", rport=2001, plist=[2001,2002,2003,2004,2005], mh=BaseMutexHandler, is_leader=False, mhargs=[False,0])
-a1 = AgentConfig(1, bots, "", rport=2002, plist=[2001,2002,2003,2004,2005], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
+a1 = AgentConfig(1, bots, "", rport=2002, plist=[2001,2002,2003,2004,2005], mh=BaseMutexHandler, is_leader=True, mhargs=[True,1])
 a2 = AgentConfig(2, bots, "", rport=2003, plist=[2001,2002,2003,2004,2005], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
 a3 = AgentConfig(3, bots, "", rport=2004, plist=[2001,2002,2003,2004,2005], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
 a4 = AgentConfig(4, bots, "", rport=2005, plist=[2001,2002,2003,2004,2005], mh=BaseMutexHandler, is_leader=False, mhargs=[False,4])
