322f8f65489de1683ea0b0bb10d729c6eeacc577
ritwika314 <mim.ghosh@gmail.com>
2019-08-26 16:47:31 -0500

    testing

--- src/apps/taskwithpaths.py
+++ src/apps/taskwithpaths.py
@@ -1,30 +1,32 @@
 import time
 
-from src.config.configs import AgentConfig
+from src.config.configs import AgentConfig, MoatConfig
 from src.harness.agentThread import AgentThread
-from src.motion.pos_types import pos3d
+from src.motion.deconflict import clear_path
 from src.objects.udt import get_tasks
 
 
 class TaskApp(AgentThread):
 
-    def __init__(self, agent_config: AgentConfig):
-        super(TaskApp, self).__init__(agent_config, None)
+    def __init__(self, agent_config: AgentConfig, m):
+        super(TaskApp, self).__init__(agent_config, m)
         self.start()
 
     def initialize_vars(self):
         self.initialize_lock('pick_route')
         self.agent_gvh.create_aw_var('tasks', list, [0 for i in get_tasks(taskfile='src/apps/tasks.txt')])
-        self.agent_gvh.create_ar_var('route', list, [None])
+        self.agent_gvh.create_ar_var('route', list, [self.agent_gvh.moat.position])
+
         self.locals['my_task'] = None
         self.locals['doing'] = False
+        self.locals['tasklist'] = get_tasks(taskfile='src/apps/tasks.txt')
         self.locals['obstacles'] = []
 
     def loop_body(self):
         if not self.locals['doing']:
             if sum(self.read_from_shared('tasks', None)) == len(
                     self.read_from_shared('tasks', None)):
-                print("print all tasks")
+                print("done all tasks")
                 self.trystop()
                 return
 
@@ -34,20 +36,34 @@ class TaskApp(AgentThread):
                 # print("Tasks are", self.locals['tasks'])
                 for i in range(len(self.locals['tasks'])):
                     if not self.locals['tasks'][i] == 1:
-                        self.locals['my_task'] = self.locals['tasks'][i]
-                        print("going to task", i)
                         self.locals['doing'] = True
-                        self.locals['my_task'] = i
-                        self.locals['tasks'][i] = 1
-                        path = [pos3d(2., 1., 0.), pos3d(-2., 1., 0.), pos3d(2., -1., 0.), pos3d(2., 1., 0.),
-                                pos3d(-2., 1., 6.), pos3d(2., -6., 0.)]
-
-                        self.agent_gvh.put('tasks', self.locals['tasks'])
-                        self.agent_gvh.put('route', path, self.pid())
+                        self.locals['my_task'] = self.locals['tasklist'][i]
+                        self.locals['test_route'] = self.agent_gvh.moat.planner.find_path(self.agent_gvh.moat.position,
+                                                                                          self.locals[
+                                                                                              'my_task'].location,
+                                                                                          [])
+                        if clear_path([path for path in
+                                       [self.read_from_shared('route', pid) for pid in range(self.num_agents())]],
+                                      self.locals['test_route'], self.pid(), tolerance=0.75):
+                            print("going to task", i)
+                            self.locals['doing'] = True
+                            self.locals['tasks'][i] = 1
+                            self.agent_gvh.put('tasks', self.locals['tasks'])
+                            self.agent_gvh.put('route', self.locals['test_route'], self.pid())
+                            self.agent_gvh.moat.follow_path(self.locals['test_route'])
+                        else:
+                            self.agent_gvh.put('route', [self.agent_gvh.moat.position],
+                                               self.pid())
+                            self.locals['my_task'] = None
+                            self.locals['doing'] = False
+                            continue
                         self.unlock('pick_route')
+                        time.sleep(0.5)
                         break
         else:
-            if self.locals['my_task'] is not None:
-                self.locals['my_task'] = None
-            self.locals['doing'] = False
-            return
+            if self.agent_gvh.moat.reached:
+                if self.locals['my_task'] is not None:
+                    self.locals['my_task'] = None
+                self.locals['doing'] = False
+                time.sleep(0.5)
+                return
