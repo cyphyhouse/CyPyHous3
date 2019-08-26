f6251217267bfb613323c23bcdf2d43b65b68209
ritwika314 <mim.ghosh@gmail.com>
2019-08-26 10:59:13 -0500

    update

---
+++ src/apps/taskwithoutgoing.py
@@ -0,0 +1,46 @@
+import time
+
+from src.config.configs import AgentConfig
+from src.harness.agentThread import AgentThread
+from src.objects.udt import get_tasks
+
+
+class TaskApp(AgentThread):
+
+    def __init__(self, agent_config: AgentConfig):
+        super(TaskApp, self).__init__(agent_config, None)
+        self.start()
+
+    def initialize_vars(self):
+        self.initialize_lock('pick_route')
+        self.agent_gvh.create_aw_var('tasks', list, [0 for i in get_tasks(taskfile='src/apps/tasks.txt')])
+        self.locals['my_task'] = None
+        self.locals['doing'] = False
+        self.locals['obstacles'] = []
+
+    def loop_body(self):
+        if not self.locals['doing']:
+            if sum(self.read_from_shared('tasks', None)) == len(
+                    self.read_from_shared('tasks', None)):
+                self.stop()
+                return
+
+            if self.lock('pick_route'):
+                time.sleep(0.4)
+                self.locals['tasks'] = self.read_from_shared('tasks', None)
+                #print("Tasks are", self.locals['tasks'])
+                for i in range(len(self.locals['tasks'])):
+                    if not self.locals['tasks'][i] == 1:
+                        self.locals['my_task'] = self.locals['tasks'][i]
+                        print("going to task", i)
+                        self.locals['doing'] = True
+                        self.locals['my_task'] = i
+                        self.locals['tasks'][i] = 1
+                        self.agent_gvh.put('tasks', self.locals['tasks'])
+                        self.unlock('pick_route')
+                        break
+        else:
+            if self.locals['my_task'] is not None:
+                self.locals['my_task'] = None
+            self.locals['doing'] = False
+            return
--- test4.py
+++ test4.py
@@ -1,4 +1,5 @@
 from src.apps.addnums import AddNums
+from src.apps.taskwithoutgoing import TaskApp
 from src.config.configs import AgentConfig
 from src.functionality.base_mutex_handler import BaseMutexHandler
 
@@ -8,4 +9,5 @@ a1 = AgentConfig(1, bots, "", rport=2001, plist=[2001,2002,2003,2004], mh=BaseMu
 a2 = AgentConfig(2, bots, "", rport=2002, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
 a3 = AgentConfig(3, bots, "", rport=2003, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
 a4 = AgentConfig(4, bots, "", rport=2004, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,4])
-app4 = AddNums(a4)
+#app4 = AddNums(a4)
+app4 = TaskApp(a4)
