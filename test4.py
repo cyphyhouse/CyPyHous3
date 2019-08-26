02a6aa939249025ba1947eb3e4f8cafeef00091d
ritwika314 <mim.ghosh@gmail.com>
2019-08-26 10:48:57 -0500

    Testing init sync

--- test4.py
+++ test4.py
@@ -1,23 +1,11 @@
-from src.apps.taskapp import TaskApp
-from src.config.configs import AgentConfig, default_qc_moat_config, default_car_moat_config
+from src.apps.addnums import AddNums
+from src.config.configs import AgentConfig
 from src.functionality.base_mutex_handler import BaseMutexHandler
-from src.motion.rrt_drone import RRT as RRT_DRONE
-from src.motion.rrt_car import RRT as RRT_CAR
-
 
 bots = 4
 
-a1 = AgentConfig(1, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
-a2 = AgentConfig(2, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
-a3 = AgentConfig(3, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
-a4 = AgentConfig(4, bots, "", rport=2001, plist=[], mh=BaseMutexHandler, is_leader=False, mhargs=[False,4])
-m0 = default_car_moat_config('hotdec_car')
-m1 = default_car_moat_config('f1car')
-m2 = default_qc_moat_config('cyphyhousecopter')
-m3 = default_qc_moat_config('cyphyhousecopter1')
-m0.planner = RRT_CAR(goal_sample_rate=15)
-m1.planner = RRT_CAR(goal_sample_rate=15)
-m2.planner = RRT_DRONE()
-m3.planner = RRT_DRONE()
-
-app4 = TaskApp(a4)
+a1 = AgentConfig(1, bots, "", rport=2001, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
+a2 = AgentConfig(2, bots, "", rport=2002, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
+a3 = AgentConfig(3, bots, "", rport=2003, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
+a4 = AgentConfig(4, bots, "", rport=2004, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,4])
+app4 = AddNums(a4)
