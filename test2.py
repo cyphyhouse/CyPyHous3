c6ce145a39193551894ab953e6583eeb3e035212
ritwika314 <mim.ghosh@gmail.com>
2019-08-26 14:33:46 -0500

    tesing

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
 from src.apps.addnums import AddNums
-from src.apps.taskwithpaths import TaskApp
+from src.apps.taskwithoutgoing import TaskApp
 from src.config.configs import AgentConfig
 from src.functionality.base_mutex_handler import BaseMutexHandler
 
-bots = 2
+bots = 4
 
 a1 = AgentConfig(1, bots, "", rport=2001, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
 a2 = AgentConfig(2, bots, "", rport=2002, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
