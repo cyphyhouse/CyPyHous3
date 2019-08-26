7c73267bb3b8858c2da50d764e294011981deca7
ritwika314 <mim.ghosh@gmail.com>
2019-08-26 13:40:07 -0500

    update

--- test2.py
+++ test2.py
@@ -9,5 +9,5 @@ a1 = AgentConfig(1, bots, "", rport=2001, plist=[2001,2002,2003,2004], mh=BaseMu
 a2 = AgentConfig(2, bots, "", rport=2002, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
 a3 = AgentConfig(3, bots, "", rport=2003, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,3])
 a4 = AgentConfig(4, bots, "", rport=2004, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,4])
-app2 = AddNums(a2)
-#app2 = TaskApp(a2)
+#app2 = AddNums(a2)
+app2 = TaskApp(a2)
