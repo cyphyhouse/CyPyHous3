847aa8b45bc7f23fd180047f0cda3967b572ef71
ritwika314 <mim.ghosh@gmail.com>
2019-08-26 14:32:10 -0500

    update

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ from src.apps.taskwithpaths import TaskApp
 from src.config.configs import AgentConfig
 from src.functionality.base_mutex_handler import BaseMutexHandler
 
-bots = 4
+bots = 2
 
 a1 = AgentConfig(1, bots, "", rport=2001, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=False, mhargs=[False,1])
 a2 = AgentConfig(2, bots, "", rport=2002, plist=[2001,2002,2003,2004], mh=BaseMutexHandler, is_leader=True, mhargs=[True,2])
