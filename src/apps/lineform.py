84e3e1ec8448e142dc782bcae9c689a45cb1568f
Hsieh, Chiao <chsieh16@illinois.edu>
2019-09-03 17:28:32 -0500

    Update lineform for cymulator

--- src/apps/lineform.py
+++ src/apps/lineform.py
@@ -7,14 +7,14 @@ class LineForm(AgentThread):
 
     def __init__(self, agent_config: AgentConfig, moat_config: MoatConfig):
         super(LineForm, self).__init__(agent_config, moat_config)
-        self.start()
 
     def initialize_vars(self):
         self.agent_gvh.create_ar_var('mypos', type(pos3d), self.agent_gvh.moat.position)
-        self.locals['tries'] = 1
 
     def loop_body(self):
         self.write_to_shared('mypos', self.pid(), self.agent_gvh.moat.position)
-        if not (self.pid() == 0 or self.pid() == self.num_agents() - 1):
-            self.agent_gvh.moat.goTo(mid_pt(self.read_from_shared('mypos', self.pid() - 1), self.read_from_shared('mypos', self.pid() + 1)))
-            self.locals['i'] += 1
+        if (self.pid() != 0 and self.pid() != self.num_agents() - 1):
+            self.agent_gvh.moat.goTo(
+                mid_pt(self.read_from_shared('mypos', self.pid() - 1),
+                       self.read_from_shared('mypos', self.pid() + 1)))
+
