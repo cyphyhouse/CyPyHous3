84546223c78b1515bfd9d3a42dbc8184aa148c07
Hsieh, Chiao <chsieh16@illinois.edu>
2019-09-03 17:29:19 -0500

    Update shapeform for cymulator

--- src/apps/shapeform.py
+++ src/apps/shapeform.py
@@ -5,27 +5,26 @@ from src.harness.agentThread import AgentThread
 from src.motion.pos_types import pos3d
 
 
-class Shapeform(AgentThread):
+class ShapeForm(AgentThread):
 
     def __init__(self, agent_config: AgentConfig, moat_config: MoatConfig):
-        super(Shapeform, self).__init__(agent_config, moat_config)
-        self.start()
+        super(ShapeForm, self).__init__(agent_config, moat_config)
 
     def initialize_vars(self):
         self.agent_gvh.create_ar_var('mypos', type(pos3d), self.agent_gvh.moat.position)
-        self.locals['tries'] = 1
-        self.locals['gridmap'] = get_gridmap(self.num_agents())
-        self.locals['corners'] = get_corners(self.num_agents())
-        self.locals['nbrs'] = get_nbrs(self.pid(),self.locals['gridmap'])
 
     def loop_body(self):
+        if self.num_agents() not in [x**2 for x in range(1, 6)]:
+            self.trystop()  # Avoid non square number of agents
+            return
         self.write_to_shared('mypos', self.pid(), self.agent_gvh.moat.position)
-        if not self.pid() in self.locals['corners']:
-            self.agent_gvh.moat.goTo(mid_pt_of_list([self.read_from_shared('mypos', i) for i in self.locals['nbrs']]))
-            self.locals['tries'] += 1
-
-        if self.locals['tries'] == 100:
-            self.trystop()
+        if self.pid() in get_corners(self.num_agents()):
+            self.agent_gvh.moat.goTo(get_corner_pos(self.pid(), self.num_agents()))
+        else:
+            self.agent_gvh.moat.goTo(
+                mid_pt_of_list(
+                    [self.read_from_shared('mypos', i) for i in get_nbrs(self.pid(), self.num_agents())]
+                ))
 
 
 def mid_pt_of_list(poslist):
@@ -34,47 +33,37 @@ def mid_pt_of_list(poslist):
 
 
 def get_corners(n):
-    corners = []
-    if math.sqrt(n).is_integer():
-        max = int(math.sqrt(n)) - 1
-        min = 0
-        corners = [(min,min), (min,max), (max,min), (max,max)
+    assert math.sqrt(n).is_integer()
+
+    w = int(math.sqrt(n))
+    corner_coord = [(0, 0), (0, w-1), (w-1, 0), (w-1, w-1)]
+    corners = set([x*w + y for x, y in corner_coord])
     return corners
 
-def get_gridmap(n):
-    i = 0
-    gridmap = []
-    if math.sqrt(n).is_integer():
-        max = int(math.sqrt(n))
-        min = 0
-        for j in range(min, max ):
-            for k in range(min,max):
-                gridmap.append((i, (j, k)))
-                i = i + 1
-        return gridmap
 
-    return None
+def get_corner_pos(pid, num_agents):
+    assert math.sqrt(num_agents).is_integer()
+    w = int(math.sqrt(num_agents))
+    x, y = (pid // w, pid % w)
+
+    space = 3.5
+    return pos3d((x - (w-1) / 2)*space,
+                 (y - (w-1) / 2)*space,
+                 1)
 
 
-def pid2d(pid, gridmap):
-    for i in gridmap:
-        if pid == i[0]:
-            return i[1]
-    return None
+def get_nbrs(pid, num_agents):
+    assert math.sqrt(num_agents).is_integer()
 
+    w = int(math.sqrt(num_agents))
+    x, y = (pid // w, pid % w)
 
-def prop_nbrs(p):
-    x1 = p[0] + 1
-    x2 = p[0] - 1
-    y1 = p[1] + 1
-    y2 = p[1] - 1
-    return [(p[0], y1), (p[0], y2), (x1, p[1]), (x2, p[1])]
+    if x == 0 or x == w-1:
+        all_nbrs = [(x, y-1), (x, y+1)]
+    elif y == 0 or y == w-1:
+        all_nbrs = [(x-1, y), (x+1, y)]
+    else:
+        all_nbrs = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
 
-
-def get_nbrs(pid, gridmap):
-    p = pid2d(pid, gridmap)
-    n = prop_nbrs(p)
-    gridlist = [p[1] for p in gridmap]
-    return list(set(gridlist).intersection(n))
-
-
+    prop_nbrs = [(nx, ny) for nx, ny in all_nbrs if 0 <= x < w and 0 <= y < w]
+    return set([nx*w + ny for nx, ny in prop_nbrs])
